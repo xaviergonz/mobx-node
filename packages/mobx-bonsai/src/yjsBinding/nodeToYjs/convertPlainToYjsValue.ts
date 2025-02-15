027e5e5bccb230cde35f679649ca4a442316d9b0
xaviergonz <xaviergonz@gmail.com>
2025-02-15 20:14:48 +0100

    improve y.js types a bit

--- packages/mobx-bonsai/src/yjsBinding/nodeToYjs/convertPlainToYjsValue.ts
+++ packages/mobx-bonsai/src/yjsBinding/nodeToYjs/convertPlainToYjsValue.ts
@@ -1,9 +1,9 @@
+import { action, isObservableArray, isObservableObject, runInAction } from "mobx"
 import * as Y from "yjs"
-import { Primitive } from "../../plainTypes/types"
 import { failure } from "../../error/failure"
 import { isPlainObject, isPrimitive } from "../../plainTypes/checks"
+import { Primitive } from "../../plainTypes/types"
 import { YjsValue } from "../yjsTypes/types"
-import { isObservableArray, isObservableObject } from "mobx"
 
 /**
  * Converts a plain value to a Y.js value.
@@ -14,37 +14,41 @@ export function convertPlainToYjsValue(v: readonly any[]): Y.Array<YjsValue>
 export function convertPlainToYjsValue(v: Readonly<Record<string, any>>): Y.Map<YjsValue>
 
 export function convertPlainToYjsValue(v: any): YjsValue {
-  if (isPrimitive(v)) {
-    return v
-  }
+  return runInAction(() => {
+    if (isPrimitive(v)) {
+      return v
+    }
 
-  if (Array.isArray(v) || isObservableArray(v)) {
-    const arr = new Y.Array<YjsValue>()
-    applyPlainArrayToYArray(arr, v)
-    return arr as YjsValue
-  }
+    if (Array.isArray(v) || isObservableArray(v)) {
+      const arr = new Y.Array<YjsValue>()
+      applyPlainArrayToYArray(arr, v)
+      return arr as YjsValue
+    }
 
-  if (isPlainObject(v) || isObservableObject(v)) {
-    const map = new Y.Map<YjsValue>()
-    applyPlainObjectToYMap(map, v)
-    return map as YjsValue
-  }
+    if (isPlainObject(v) || isObservableObject(v)) {
+      const map = new Y.Map<YjsValue>()
+      applyPlainObjectToYMap(map, v)
+      return map as YjsValue
+    }
 
-  throw failure(`unsupported value type: ${v}`)
+    throw failure(`unsupported value type: ${v}`)
+  })
 }
 
 /**
  * Applies a plain array to a Y.Array, using the convertPlainToYjsValue to convert the values.
  */
-export function applyPlainArrayToYArray(dest: Y.Array<any>, source: readonly any[]) {
+export const applyPlainArrayToYArray = action((dest: Y.Array<any>, source: readonly any[]) => {
   dest.push(source.map(convertPlainToYjsValue))
-}
+})
 
 /**
  * Applies a plain object to a Y.Map, using the convertPlainToYjsValue to convert the values.
  */
-export function applyPlainObjectToYMap(dest: Y.Map<any>, source: Readonly<Record<string, any>>) {
-  Object.entries(source).forEach(([k, v]) => {
-    dest.set(k, convertPlainToYjsValue(v))
-  })
-}
+export const applyPlainObjectToYMap = action(
+  (dest: Y.Map<any>, source: Readonly<Record<string, any>>) => {
+    Object.entries(source).forEach(([k, v]) => {
+      dest.set(k, convertPlainToYjsValue(v))
+    })
+  }
+)
